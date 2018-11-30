b49f35ba00ed7e8637408abd155a3f9083037825
kate ross <kross@clever-touch.com>
2018-11-30 23:27:03 +0000

    Change to Pirate Ipsum and add body section

--- resources/views/pages/tutorial.blade.php
+++ resources/views/pages/tutorial.blade.php
@@ -18,7 +18,8 @@
                     <div class="col">
                         <h6 class="m-b-25">Lorem Ipsum</h6>
                         <h3 class="f-w-700 text-c-blue">Overview</h3>
-                        <p class="m-b-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, deleniti dignissimos eius et explicabo hic incidunt labore nesciunt porro quisquam. Et illo nam natus porro recusandae repudiandae tempore temporibus vel.</p>
+                        <p class="">Mizzen handsomely long boat bilged on her anchor no prey, no pay fluke prow gangway avast Admiral of the Black bowsprit Jolly Roger bilge water hail-shot poop deck. Barque no prey, no pay yo-ho-ho scuttle swab booty blow the man down ballast knave cog yawl quarter pinnace belay coxswain. American Main lugsail lugger main sheet hulk provost topmast man-of-war belaying pin bilged on her anchor coxswain execution dock aft Shiver me timbers spike.</p>
+                        <p class="m-b-0">Snow Privateer aft sloop gangplank careen gabion handsomely avast cable Cat o'nine tails boom draught ho lateen sail. Dead men tell no tales aft flogging nipperkin hempen halter swab grapple six pounders cable wench maroon aye come about league scurvy. Sink me yo-ho-ho ye nipperkin deadlights six pounders log plunder belay gibbet gun lad Arr knave warp.</p>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-eye bg-c-blue"></i>
@@ -26,5 +27,26 @@
                 </div>
             </div>
         </div>
+    </div>
+@endsection
+
+@section('body')
+    <div class="col-md-12 col-xl-4">
+        <div class="card comp-card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="m-b-25 text-c-blue">Example</h3>
+                        <h6 class="f-w-700">Overview</h6>
+                        <p class="">Mizzen handsomely long boat bilged on her anchor no prey, no pay fluke prow gangway avast Admiral of the Black bowsprit Jolly Roger bilge water hail-shot poop deck. Barque no prey, no pay yo-ho-ho scuttle swab booty blow the man down ballast knave cog yawl quarter pinnace belay coxswain. American Main lugsail lugger main sheet hulk provost topmast man-of-war belaying pin bilged on her anchor coxswain execution dock aft Shiver me timbers spike.</p>
+
+                        <pre><code>
+                            adsfljaks
+                            </code></pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
